2b7a15078b80dbcb9908adb0575308c3f610e2d5
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-01 16:17:11 +0200

    Create allowance.entry.js

---
+++ allowance.entry.js
@@ -0,0 +1,13 @@
+/**
+ * HCM Autoload Allowance Field
+ *
+ * This file intentionally:
+ * - imports nothing by name
+ * - exports nothing
+ * - references nothing
+ *
+ * It simply allows everything that exists
+ * to be loaded.
+ */
+
+import.meta.glob('./**/*.js', { eager: true });
